431cd29a1fd11b9d244205258c06f196b7642b36
A <gavin.k.armstrong@gmail.com>
2020-10-05 00:08:48 -0600

    Looking at available data.

--- TikTok_API_Setup.py
+++ TikTok_API_Setup.py
@@ -1,11 +1,13 @@
 from TikTokApi import TikTokApi
 api = TikTokApi()
 
-results = 10
+results = 1
 
 trending = api.trending(count=results)
 
-for tiktok in trending:
-    print(tiktok['desc'].encode("utf-8"))
+# for tiktok in trending:
+#     print(tiktok['desc'].encode("utf-8"))
 
-print(len(trending))
+# print(len(trending))
+
+print(trending[0].keys())
