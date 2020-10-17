afad5348c54e5525ce8fd3cf441c49cbfff0a602
A <gavin.k.armstrong@gmail.com>
2020-10-16 20:49:48 -0600

    Reading in data.

--- TikTok Import Data.py
+++ TikTok Import Data.py
@@ -4,9 +4,19 @@ from os import listdir
 from os.path import isfile, join
 
 # List the files in te TikTok_DB directory
-path = "TikTok_DB"
+path = "Test"
 
 files = [item for item in listdir(path=path) if isfile(join(path, item))]
 
+for f in files:
+    print(f)
 
-print(files)
+# with open('test.json') as g:
+#     read_data = g.read()
+
+# print(read_data)
+
+import os
+path = os.getcwd()
+
+print(path)
