995487636632e716f83cf2854ad641f78b29d1ac
A <gavin.k.armstrong@gmail.com>
2020-10-17 21:14:26 -0600

    Reading and printing file.

--- TikTok Import Data.py
+++ TikTok Import Data.py
@@ -11,12 +11,14 @@ files = [item for item in listdir(path=path) if isfile(join(path, item))]
 for f in files:
     print(f)
 
-# with open('test.json') as g:
-#     read_data = g.read()
-
-# print(read_data)
-
 import os
 path = os.getcwd()
 
 print(path)
+
+file = join(path, 'test.json')
+print(file)
+
+with open('Test/test.json') as g:
+    read_data = g.read()
+    print(read_data)
