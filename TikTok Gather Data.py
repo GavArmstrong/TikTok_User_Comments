ae89fdba1647f797bd655b821ab669d99176861e
A <gavin.k.armstrong@gmail.com>
2020-10-25 23:31:33 -0600

    Concatenating jsons.

--- TikTok Gather Data.py
+++ TikTok Gather Data.py
@@ -6,19 +6,26 @@ from re import sub
 
 api = TikTokApi()
 
-results = 5
+results = 2
 
 file_iter = datetime.now()
 file_iter = file_iter.strftime("%d/%m/%Y %H:%M:%S")
 file_iter = sub("(/)|(:)|( )", "", file_iter)
 
 f = open("TikTok_DB/DB_" + file_iter + ".json", "w")
+f.write("{")
 
-for j in range(20):
+for j in range(1):
     trending = api.trending(count=results, request_delay=uniform(0.5,1.5))
 
-    for i in trending:
-        json_obj = json.dumps(i)
+    for i in range(len(trending)):
+        f.write("{\"id\":")
+        f.write(trending[i]['id'])
+        f.write("\n")
+        json_obj = json.dumps(trending[i])
         f.write(json_obj)
+        if i < len(trending) - 1:
+            f.write(",\n")
 
+f.write("}")
 f.close()
