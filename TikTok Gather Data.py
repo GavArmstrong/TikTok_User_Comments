52a0936d72200215be7c400b21bda79822c3d3b6
A <gavin.k.armstrong@gmail.com>
2020-10-29 21:12:07 -0600

    Changed file name structure.

--- TikTok Gather Data.py
+++ TikTok Gather Data.py
@@ -6,27 +6,24 @@ from re import sub
 
 api = TikTokApi()
 
-results = 2
+results = 10
 
 file_iter = datetime.now()
-file_iter = file_iter.strftime("%d/%m/%Y %H:%M:%S")
+file_iter = file_iter.strftime("%Y/%m/%d %H:%M:%S")
 file_iter = sub("(/)|(:)|( )", "", file_iter)
 
 f = open("TikTok_DB/DB_" + file_iter + ".json", "w")
 f.write("{")
 
-for j in range(1):
+for j in range(10):
     trending = api.trending(count=results, request_delay=uniform(0.5,1.5))
 
     for i in range(len(trending)):
-        # f.write("{\"id\": \"")
         f.write("\"")
         f.write(trending[i]['id'])
-        # f.write("\",\n\"TikTok\": ")
         f.write("\": ")
         json_obj = json.dumps(trending[i])
         f.write(json_obj)
-        # f.write("}")
         if i < len(trending) - 1:
             f.write(",\n")
 
